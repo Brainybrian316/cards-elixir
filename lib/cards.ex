44d94314926fd4f5c0199c3eaa1ce3bfae7df56f
Brainybrian316 <brainybrian316@gmail.com>
2022-10-03 14:49:42 -0400

    moved files to root

--- lib/cards.ex
+++ lib/cards.ex
@@ -24,6 +24,11 @@ defmodule Cards do
     Enum.split(deck, hand_size)
   end
 
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
  def shuffle(deck) do
     Enum.shuffle(deck)
  end
